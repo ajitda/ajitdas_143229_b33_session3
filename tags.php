da1b9b0be122adb8266b08053d3e0f47c6152992
Ajit Das <ajitdas2900@gmail.com>
2016-09-19 10:32:27 +0600

    new file added

---
+++ tags.php
@@ -0,0 +1,24 @@
+<?php
+echo 'I am inside Php standard tag';
+
+?>
+<br>
+<?
+
+echo 'This is Short Open text';
+
+?>
+<br>
+
+<%
+
+echo 'this is asp tag';
+
+%>
+<br>
+
+
+<script language="php">
+
+    echo 'this is script tags';
+</script>
